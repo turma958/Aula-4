2a4c11cb6bef6e15e32d10212ac4cc7e80d448ad
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2022-12-14 21:05:57 -0300

    Refatoração e utilização de classes parciais

--- Aula 4/Program.cs
+++ Aula 4/Program.cs
@@ -1,49 +1,29 @@
-﻿namespace Aula_4
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aula_4
 {
-    public class Conversor10toX
+    public partial class Conversor10toX
     {
         static void Main(string[] args)
         {
             int numBase10 = 20;
-            List<string> numBaseX = new List<string>();
-            Dictionary<int, string> algarimosTraduzidos = new Dictionary<int, string>()
-            {
-                {0,"0"},
-                {1,"1"},
-                {2,"2"},
-                {3,"3"},
-                {4,"4"},
-                {5,"5"},
-                {6,"6"},
-                {7,"7"},
-                {8,"8"},
-                {9,"9"},
-                {10,"A"},
-                {11,"B"},
-                {12,"C"},
-                {13,"D"},
-                {14,"E"},
-                {15,"F"},
-            };
-            int BaseX = 2;
+            var numBaseX = new Stack<string>();
 
+            int BaseX = 2;
             while (numBase10 > 0)
             {
                 int resto = numBase10 % BaseX;
                 numBase10 = numBase10 / BaseX;
-                numBaseX.Add(algarimosTraduzidos[resto]);
+                numBaseX.Push(algarimosTraduzidos[resto]);
             }
 
-            numBaseX.Reverse();
-            if (numBaseX[0] == "0")
-                numBaseX.RemoveAt(0);
-
-            foreach (string num in numBaseX)
-            {
-                Console.Write($"{num}");
-            }
+            if (numBaseX.Peek() == "0")
+                numBaseX.Pop();
 
 
+            var numero = String.Join("", numBaseX);
+            Console.WriteLine(numero);
         }
     }
 }
---
+++ Aula 4/ProgramConstants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_4
+{
+    public partial class Conversor10toX
+    {
+        static Dictionary<int, string> algarimosTraduzidos = new Dictionary<int, string>()
+        {
+            {0,"0"},
+            {1,"1"},
+            {2,"2"},
+            {3,"3"},
+            {4,"4"},
+            {5,"5"},
+            {6,"6"},
+            {7,"7"},
+            {8,"8"},
+            {9,"9"},
+            {10,"A"},
+            {11,"B"},
+            {12,"C"},
+            {13,"D"},
+            {14,"E"},
+            {15,"F"},
+        };
+    }
+}
